998e243111c9b7549dd1eca18365635d64016278
Alexei Chebanu <0alexchebanu@gmail.com>
2022-12-27 21:53:15 +0200

    Migrations

--- Entities/PersonDbContext.cs
+++ Entities/PersonDbContext.cs
@@ -9,5 +9,42 @@ namespace Entities
 {
     public class PersonDbContext : DbContext
     {
+        public PersonDbContext(DbContextOptions options) : base (options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Person> Persons { get; set;}
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Country>().ToTable("Countries");
+
+            modelBuilder.Entity<Person>().ToTable("Persons");
+
+            //Seed to Countries
+            string countriesJSON = File.ReadAllText("countries.json");
+
+           List<Country>? countries = System.Text.Json.JsonSerializer.Deserialize<List<Country>>(countriesJSON);
+
+
+            foreach (Country country in countries)
+            {
+                modelBuilder.Entity<Country>().HasData(country);
+            }
+
+            string personsJSON = File.ReadAllText("person.json");
+
+            List<Person>? persons = System.Text.Json.JsonSerializer.Deserialize<List<Person>>(personsJSON);
+
+
+            foreach (Person person in persons)
+            {
+                modelBuilder.Entity<Person>().HasData(person);
+            }
+
+        }
     }
 }
